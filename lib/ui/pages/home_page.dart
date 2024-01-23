ab999c40e61bdf00bb5b9d4f97099903fc2c97e4
afrizzal <apratama8@gmail.com>
2024-01-24 00:26:28 +0700

    homepage update

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -77,6 +77,11 @@ class HomePage extends StatelessWidget {
           horizontal: 24,
         ),
         children: [
+          buildProfile(),
+          buildWalletCard(),
+          // const SizedBox(
+          //   height: 40,
+          // ),
         ],
       ),
     );
@@ -87,8 +92,116 @@ class HomePage extends StatelessWidget {
         top: 40,
         ),
         child: Row(
-        children:[],      
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children:[
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Howdy,',
+                style: greyTextStyle.copyWith(
+                  fontSize: 16,
+                ),
+              ),
+              const SizedBox(
+                height: 2,
+              ),
+              Text(
+                'User',
+                style: blackTextStyle.copyWith(
+                  fontSize: 20,
+                  fontWeight: semiBold,
+                ),
+              ),
+            ],
+          ),
+          Container(
+            width: 60,
+            height: 60,
+            decoration: const BoxDecoration(
+              shape: BoxShape.circle,
+              image: DecorationImage(
+                image:AssetImage(
+                  'assets/img_profile.png',
+                ),
+              ),
+            ),
+            child: Align(
+              alignment: Alignment.topRight,
+              child: Container(
+                width: 16,
+                height: 16,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: whiteColor,
+                ),
+                child: Icon(
+                Icons.check_circle,
+                color: greenColor,
+                            ),
+              ),
+          ),
         ),
+      ],      
+    ),
+  );
+}
+
+  Widget buildWalletCard() {
+    return Container(
+      width: double.infinity,
+      height: 220,
+      margin: const EdgeInsets.only(
+        top: 30,
+      ),
+      padding: const EdgeInsets.all(30),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(28),
+        image: const DecorationImage(
+          fit: BoxFit.cover,
+          image: AssetImage(
+            'assets/img_bg_card.png',
+          ),
+        ),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Reza Choiruddin',
+            style: whiteTextStyle.copyWith(
+              fontSize: 18,
+              fontWeight: medium,
+            ),
+          ),
+          const SizedBox(
+            height: 28,
+          ),
+          Text(
+            '**** **** 1234',
+            style: whiteTextStyle.copyWith(
+              fontSize: 18,
+              fontWeight: medium,
+              letterSpacing: 6,
+            ),
+          ),
+          const SizedBox(
+            height: 21,
+          ),
+          Text(
+            'Balance',
+            style: whiteTextStyle,
+            ),
+            Text(
+              'IDR 666',
+              style: whiteTextStyle.copyWith(
+                fontSize: 24,
+                fontWeight: semiBold,
+              ),
+              
+              )
+        ],
+      ),
     );
   }
 }
